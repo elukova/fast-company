1f6d0077cf9ebe2f45285fcd12a6b965fd35e0b9
Завальнюк Денис Андреевич <zavalnyuk.denis@tseh85.ru>
2023-06-08 12:54:57 +0300

    routing hw

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import User from "./user";
 import UsersList from "./usersList";
 import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
 
-const Users = ({ match }) => {
-  const userId = match.params.userId;
+const Users = () => {
+  const params = useParams();
+  const { userId } = params;
   return <>{userId ? <User userId={userId} /> : <UsersList />}</>;
 };
 
